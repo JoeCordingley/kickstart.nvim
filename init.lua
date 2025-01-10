3509b9713ef63edae55422f6c57e248242a3b762
JoeCordingley <joe@txodds.com>
2025-01-10 21:33:38 +0000

    merge

--- init.lua
+++ init.lua
@@ -170,6 +170,12 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+-- Associate .purs files with the purescript filetype
+vim.filetype.add {
+  extension = {
+    purs = 'purescript',
+  },
+}
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
@@ -576,6 +582,7 @@ require('lazy').setup({
         -- rust_analyzer = {},
         hls = {},
         jsonls = {},
+        -- purescriptls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -791,35 +798,35 @@ require('lazy').setup({
       }
     end,
   },
-  --  {
-  --    'maxmx03/solarized.nvim',
-  --    lazy = false,
-  --    priority = 1000,
-  --    config = function()
-  --      vim.o.background = 'dark' -- or 'light'
-  --
-  --      vim.cmd.colorscheme 'solarized'
-  --    end,
-  --  },
+  {
+    'maxmx03/solarized.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.o.background = 'dark' -- or 'light'
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'ishan9299/nvim-solarized-lua',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       vim.cmd.colorscheme 'solarized'
-      vim.o.background = 'dark'
-
-      -- You can configure highlights by doing something like:
-      --vim.cmd.hi 'Comment gui=none'
     end,
   },
+
+  -- { -- You can easily change to a different colorscheme.
+  --   -- Change the name of the colorscheme plugin below, and then
+  --   -- change the command in the config to whatever the name of that colorscheme is.
+  --   --
+  --   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  --   'ishan9299/nvim-solarized-lua',
+  --   priority = 1000, -- Make sure to load this before all the other start plugins.
+  --   init = function()
+  --     -- Load the colorscheme here.
+  --     -- Like many other themes, this one has different styles, and you could load
+  --     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+  --     vim.cmd.colorscheme 'solarized'
+  --     vim.o.background = 'dark'
+
+  --     -- You can configure highlights by doing something like:
+  --     --vim.cmd.hi 'Comment gui=none'
+  --   end,
+  -- },
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
@@ -1001,9 +1008,11 @@ require('lazy').setup({
     },
     config = true,
   },
+  { 'alvan/vim-closetag' },
+  { 'github/copilot.vim' },
+  -- removed because it messed with completion
   --  {
-  --    'pmizio/typescript-tools.nvim',
-  --    dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+  --    'FabijanZulj/blame.nvim',
   --    opts = {},
   --  },
   --  {
